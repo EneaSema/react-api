79b880f307ec347c7a625cbdb9465c292ee3d617
EneaSema <sema.enea1993@gmail.com>
2025-05-28 17:25:21 +0200

    problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const urlActors = "https://lanciweb.github.io/demo/api/actors/";
+
+  axios.get(urlActors).then(console.log(urlActors));
+
+
+const [] =useState([])
+useEffect(() => (),[])
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="container">
+        <h1>Cast Fetching</h1>
+        <div className="row">
+          <div className="col-1-4">
+            <div className="card">
+              <div className="card-title">title</div>
+              <div className="card-body">body</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
